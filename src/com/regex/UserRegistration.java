9be43a6e91d8c1649df7dd389e32ee456580947d
Nilofar Mujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-03-10 13:42:49 +0530

    Added a method to validate LastName

--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -1,8 +1,13 @@
 package com.regex;
 
+
 /**
  * UC1 :- As a User need to enter a valid First Name
  *        - First name starts with Cap and has minimum 3 characters
+ *
+ * UC2 :- As a User need to enter a valid Last Name
+ *        - Last name starts with Cap and has minimum 3 characters
+ * 
  */
 
 /**
