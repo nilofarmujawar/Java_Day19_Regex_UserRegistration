3e0bfe3c414868e0f6e1d230d9fdb7d061460d67
Nilofar Mujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-03-10 15:38:36 +0530

    should clear all the email samples provide separately

--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -26,7 +26,7 @@ package com.regex;
  *  A regular expression is a sequence of characters that forms a search pattern.
  *  When you search for data in a text, you can use this search pattern to describe what you are searching for.
  */
-
+ 
 /**
  * import scanner class ,matcher,pattern class
  */
